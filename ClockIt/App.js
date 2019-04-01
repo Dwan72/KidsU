b778d9df5043a7806c5d41eb7a52290966fb1279
Donghwan An <dxa150530@utdallas.edu>
2019-04-01 18:36:35 -0500

    fixed Roboto font async error

--- ClockIt/App.js
+++ ClockIt/App.js
@@ -4,7 +4,29 @@ import LoginScreenRouter from './src/LoginScreen/index'
 
 
 export default class ClockIt extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
+    });
+    this.setState({ loading: false });
+  }
+
+
   render() {
+    if (this.state.loading) {
+      return <Expo.AppLoading />;
+    }
     return <LoginScreenRouter />;
   }
+
+
+
+
+
 }
