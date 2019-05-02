f240b4b465788692d933259f923ae1af66b8a5c1
Donghwan An <dxa150530@utdallas.edu>
2019-05-02 17:21:21 -0500

    changed the way the timesheet populates

---
+++ ClockIt/src/TimesheetScreen/Timecard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, TouchableOpacity } from 'react-native'
+import { Container, Header, Content, List, ListItem, Text, Separator, Right, Left, Footer, FooterTab, Button, Icon, Body, Title } from 'native-base';
+
+
+export default class TimecardClass extends React.Component{
+    render() {
+
+        <Text>What's up guys</Text>
+    }
+}
--- ClockIt/src/TimesheetScreen/TimesheetScreen.js
+++ ClockIt/src/TimesheetScreen/TimesheetScreen.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 import { Container, Header, Content, List, ListItem, Text, Separator, Right, Left, Footer, Button, Icon, Body, Title } from 'native-base';
 import { Ionicons } from '@expo/vector-icons';
 import { createStackNavigator } from 'react-navigation'
+import { View, StyleSheet, TouchableOpacity } from 'react-native'
+
+const times = [
+  {Day: 'March 1', time1: '5:03PM', time2: '10:20PM'},
+  {Day: 'March 1', time1: '5:03PM', time2: '10:20PM'},
+  {Day: 'March 1', time1: '5:03PM', time2: '10:20PM'},
+  {Day: 'March 1', time1: '5:03PM', time2: '10:20PM'},
+  {Day: 'March 1', time1: '5:03PM', time2: '10:20PM'},
+  {Day: 'March 1', time1: '5:03PM', time2: '10:20PM'},
+  {Day: 'March 1', time1: '5:03PM', time2: '10:20PM'},
+  {Day: 'March 1', time1: '5:03PM', time2: '10:20PM'},
+  {Day: 'March 1', time1: '5:03PM', time2: '10:20PM'},
+  {Day: 'March 1', time1: '5:03PM', time2: '10:20PM'},
+]
 
 
 export default class ListSeparatorExample extends React.Component{
@@ -10,8 +24,6 @@ export default class ListSeparatorExample extends React.Component{
     header: null
   }
   
-  
-
   render() {
 
     const { navigate } = this.props.navigation;
@@ -34,7 +46,29 @@ export default class ListSeparatorExample extends React.Component{
 
       <Content>
 
-        <Separator bordered>
+        {
+          times.map((time, index) => {
+            return (
+              <View key={time}>
+                <TouchableOpacity
+                  onPress = {() => this.props.navigation.navigate('Timecard')}
+                >
+                  <ListItem style={styles.timeLabel}>
+                      <View>
+                        <Text>Shift {index+1}</Text>
+                      </View>
+                      <View>
+                        <Text>{time.time1} - {time.time2}</Text>
+                      </View>
+                  </ListItem>
+                </TouchableOpacity>
+
+              </View>
+            )
+          })
+        }
+
+        {/* <Separator bordered>
           <Text>Wed Apr 3</Text>
         </Separator>
 
@@ -62,7 +96,7 @@ export default class ListSeparatorExample extends React.Component{
 
         <ListItem last>
           <Text>Shift Total</Text>
-        </ListItem>
+        </ListItem> */}
 
       </Content>
 
@@ -70,11 +104,14 @@ export default class ListSeparatorExample extends React.Component{
 
     );
   }
-
-
 }
-
-
+const styles = StyleSheet.create({
+  timeLabel:{
+      flexDirection: 'row',
+      flex: 1,
+      justifyContent: 'space-between'
+  },
+})
 
 
 
